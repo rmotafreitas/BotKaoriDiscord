b00cfc2d1549584f1d11a1387256f26607a89e4a
Ricardo Freitas <userpoke13@gmail.com>
2020-08-01 14:44:19 +0100

    hack para infos

--- src/commands/infos.js
+++ src/commands/infos.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-<<<<<<< HEAD
 const moment = require("moment");
 const flags = {
   DISCORD_EMPLOYEE: "Discord Employee",
@@ -140,34 +139,5 @@ const execute = async (bot, msg, [target]) => {
 module.exports = {
   name: "infos",
   help: "Ele mostra as info de um user",
-=======
-
-const execute = async (bot, msg, args) => {
-  let sobre = [
-    "**❯Bot feito por BestNessPT#4289**",
-    `**❯Para saber mais faça $help**\n`,
-    `**❯Estou atualmente em ${bot.guilds.cache.size} servers**`,
-    `**❯Gostou de mim? [Convide-me para o seu server!](${"https://discord.com/api/oauth2/authorize?client_id=730092279326441574&permissions=8&scope=bot"})**`,
-  ];
-
-  let Embed = new MessageEmbed()
-    .setAuthor(
-      "Kaori Miyazono#5192",
-      "https://cdn.discordapp.com/avatars/730092279326441574/12b7f197efcfaee9b8740f80d40fe491.webp?size=256"
-    )
-    .setTitle("Sobre mim! ^^")
-    .setThumbnail(
-      "https://cdn.discordapp.com/avatars/730092279326441574/12b7f197efcfaee9b8740f80d40fe491.webp?size=256"
-    )
-    .setDescription(sobre)
-    .setColor(`RANDOM`);
-  msg.delete().catch((O_o) => {});
-  msg.channel.send(Embed);
-};
-
-module.exports = {
-  name: "info",
-  help: "Ele mostra as suas info!",
->>>>>>> 5253e96acc9c81165e91cb72d1e4cd2fbb33ae8f
   execute,
 };
