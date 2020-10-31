f18fc3941ef351c5248d703341c350c1dd778808
Ricas <userpoke13@gmail.com>
2020-10-31 15:18:55 +0100

    invite bug

--- src/commands/management/invite.js
+++ src/commands/management/invite.js
@@ -2,9 +2,8 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const execute = async (bot, msg, args) => {
   var kaori = bot.users.cache.get("730092279326441574");
-  let kaoriavatar = kaori.displayAvatarURL({ size: 4096, dynamic: true });
+  let kaoriavatar = bot.users.cache.get("730092279326441574").displayAvatarURL({ size: 4096, dynamic: true });
   var dev = bot.users.cache.get("513113161126248469");
-  let devavatar = dev.displayAvatarURL({ size: 4096, dynamic: true });
   let Emebed = new MessageEmbed()
     .setAuthor("Kaori Miyazono#5192", kaoriavatar)
     .setColor("RANDOM")
