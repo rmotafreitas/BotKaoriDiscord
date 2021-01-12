39185b5ee7081f01745a9a58b9d751526a2291d6
Ness.js <68559739+rmotafreitas@users.noreply.github.com>
2021-01-12 00:43:16 +0000

    Update waifu.js

--- src/commands/anime/waifu.js
+++ src/commands/anime/waifu.js
@@ -47,7 +47,7 @@ const execute = async (bot, msg, args) => {
   });
 };
 module.exports = {
-  name: "waifu",
+  name: "w",
   section: "<:yay:764881220773216297> Anime",
   help: "Shows a waifu",
   usage: "waifu",
