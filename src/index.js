89a74a7df8e763f0053f0288210f847df8f7eb6f
Ricas <userpoke13@gmail.com>
2021-02-05 04:30:46 +0100

    keep alive heroku

--- src/index.js
+++ src/index.js
@@ -89,3 +89,30 @@ bot.on("guildCreate", (guild) => {
 bot.on("guildDelete", (guild) => {
   guildDelete(bot, guild);
 });
+
+// keepAlive.js
+const fetch = require('node-fetch');
+
+// globals
+const interval = 25*60*1000; // interval in milliseconds - {25mins x 60s x 1000}ms
+const url = 'https://kaori-sub.herokuapp.com/'
+
+(function wake() {
+
+  try {
+
+    const handler = setInterval(() => {
+
+      fetch(url)
+        .then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`)
+        .catch(err => console.error(`Error occured: ${err}`)));
+
+    }, interval);
+
+  } catch(err) {
+      console.error('Error occured: retrying...');
+      clearInterval(handler);
+      return setTimeout(() => wake(), 10000);
+  };
+
+})();
