f9316fac8759d1bc98561d2a426656871714e567
NessDellvalle <77896816+NessDellvalle@users.noreply.github.com>
2021-06-25 18:38:31 +0100

    url host

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ const fetch = require("node-fetch");
 
 // globals
 const interval = 25 * 60 * 1000; // interval in milliseconds - {25mins x 60s x 1000}ms
-const url = "https://kaori-sub.herokuapp.com/";
+const url = "https://bot-kaori-node.herokuapp.com";
 const lavaUrl = process.env.lava;
 wake();
 function wake() {
@@ -94,11 +94,11 @@ function wake() {
           .log(`response-ok: ${res.ok}, status: ${res.status}`)
           .catch((err) => console.error(`Error occured: ${err}`))
       );
-       fetch(lavaUrl).then((res) =>
+       /*fetch(lavaUrl).then((res) =>
         console
           .log(`response-ok: ${res.ok}, status: ${res.status}`)
           .catch((err) => console.error(`Error occured: ${err}`))
-      );
+      );*/
     }, interval);
   } catch (err) {
     console.error("Error occured: retrying...");
