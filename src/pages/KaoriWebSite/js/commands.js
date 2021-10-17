9bed7d4b1532596f223980b2342442017c7085b2
rmotafreitas <rmotafreitas@gmail.com>
2021-10-17 21:02:10 +0100

    New kaori website commands

---
+++ src/pages/KaoriWebSite/js/commands.js
@@ -0,0 +1,20 @@
+function show(evt, command) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(command).style.display = "block";
+  evt.currentTarget.className += " is-active";
+}
